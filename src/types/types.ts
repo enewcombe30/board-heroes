671068eaa91f6fea865a16af821ac24fed4554b2
elijah newcombe <78599237+enewcombe30@users.noreply.github.com>
2024-02-08 17:36:40 +0000

    Added hero types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,1 +1,16 @@
 // Input all types below
+
+export type hero = {
+  role: string;
+  armour: number;
+  damage: number;
+  healing: number;
+  health: number;
+  points: number;
+  grade: string;
+};
+
+export type heroList = {
+  class: string;
+  heroes: hero[];
+};
