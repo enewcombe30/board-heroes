c35c1f90214b26f0880d20a35302d82049d81d10
elijah newcombe <78599237+enewcombe30@users.noreply.github.com>
2024-02-21 13:07:14 +0000

    code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,6 @@
-import { useCallback, useState } from "react";
 import HeroSelector from "./components/HeroSelector";
-import { hero, heroList } from "./types/types";
-import { defaultList, defaultHero } from "./constants/heroes";
 
 function App() {
-  const [party, setParty] = useState<heroList>(defaultList);
-  const [showHeroes, setShowHeroes] = useState<boolean>(false);
-  const [hero1, setHero1] = useState<hero>(defaultHero);
-  const [hero2, setHero2] = useState<hero>(defaultHero);
-  const [hero3, setHero3] = useState<hero>(defaultHero);
-  const [hero4, setHero4] = useState<hero>(defaultHero);
-  const [hero5, setHero5] = useState<hero>(defaultHero);
-
-  const createParty = useCallback(() => {
-    const selectedHeroes: hero[] = [hero1, hero2, hero3, hero4, hero5];
-  }, [hero1, hero2, hero3, hero4, hero5]);
-
   return (
     <div className="w-full h-screen bg-[#f7f8fa] py-8 font-sans">
       <div className="w-fit text-4xl mx-auto mb-16">Hero selection</div>
